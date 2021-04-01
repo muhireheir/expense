b7c5bf34dc10eae24c46076dfd72bc12b9d99fca
Umuhire Heritier <muhire416@gmail.com>
2021-04-01 12:31:42 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './app/components/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './app/redux/'
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,3 +19,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+serviceWorker.register();
